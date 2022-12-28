68033a6a79beae27017f38bcb0e4427c540e11d7
jrpasia <121219634+jrpasia@users.noreply.github.com>
2022-12-28 00:24:13 -0600

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -18,4 +20,40 @@ public class LoginTests extends BaseTest {
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
+    @Test
+    public static void LoginValidEmailPasswordTest() throws InterruptedException {
+
+        //    Precondition: Chrome browser should be opened : DONE
+        //    Step1. Open koel login page: DONE
+        //    Step2. Enter Existing username
+        //    Step3. Enter Correct password
+        //    Step4. Click Login button
+        //    Expected result: User should be directed to the home page
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //Step1. Open koel login page
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        //Step2. Enter Existing username
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys("demo@class.com");
+
+        //Step3. Enter Correct password
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys("te$t$tudent");
+
+        //Step4. Click Login button
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+        loginButton.click();
+
+        WebElement avatar = driver.findElement(By.className("avatar"));
+        Assert.assertTrue(avatar.isDisplayed());
+
+        driver.quit();
+    }
+
 }
