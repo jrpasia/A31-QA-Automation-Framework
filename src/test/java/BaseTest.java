f4e1962cf144761af59dc1a860d5dbd0c390ecfd
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-01-09 00:03:20 -0600

    Added HW19 class

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -9,44 +9,44 @@ import java.time.Duration;
 
 public class BaseTest {
 
-//    public static Webdriver driver = null;
-//    public static String
+    public static Webdriver driver = null;
+    public static String
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
-//    @BeforeMethod
-//    @Parameters({"BaseURL"})
-//    public static void launchBrowser(String BaseURL) {
-//        LoginTests.driver =new ChromeDriver();
-//        LoginTests.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//        driver.get(BaseURL);
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+    public static void launchBrowser(String BaseURL) {
+        LoginTests.driver = new ChromeDriver();
+        LoginTests.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(BaseURL);
     }
 
-//    public static void login(String email, String password) {
-//        provideEmail(email);
-//        providePassword(password);
-//        clickSubmit();
-//    }
+    public static void login(String email, String password) {
+        provideEmail(email);
+        providePassword(password);
+        clickSubmit();
+    }
 //
-//    public static void clickSubmit() {
-//        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
-//        submitButton.click();
-//    }
-//    public static void provideEmail(String email) {
-//        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-//        emailField.clear();
-//        emailField.sendKeys(email);
-//
-//    }
-//    public static void providePassword (String password) {
-//        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-//        passwordField.clear();
-//        passwordField.sendKeys(password);
-//
-//    }
+    public static void clickSubmit() {
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+    }
+    public static void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+
+    }
+    public static void providePassword (String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+
+    }
 
 }
 
