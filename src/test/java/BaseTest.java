0709475129c4ae8ef0b263d8814a34045b0c5989
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-01-10 15:17:09 -0600

    Updated Base Test class

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,17 +3,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
 
 import java.time.Duration;
+import java.util.UUID;
 
 
 public class BaseTest {
-
-    public static Webdriver driver = null;
-    public static String url = "https://bbb.testpro.io/";
+    public static WebDriver driver = null;
+    public static String url = null;
+    public static WebDriverWait wait = null;
 
     @BeforeSuite
     static void setupClass() {
@@ -24,33 +25,80 @@ public class BaseTest {
     @Parameters({"BaseURL"})
     public static void launchBrowser(String BaseURL) {
         LoginTests.driver = new ChromeDriver();
-        LoginTests.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get(BaseURL);
+//        LoginTests.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(LoginTests.driver, Duration.ofSeconds(10));
+        url = BaseURL;
+        driver.get(url);
     }
+
+    @AfterMethod
+    public static void closeBrowser(){
+        LoginTests.driver.quit();
+    }
+
+//    protected static void navigateToPage() {
+//        String url = "https://bbb.testpro.io/";
+//        driver.get(url);
+//    }
 
     public static void login(String email, String password) {
         provideEmail(email);
         providePassword(password);
         clickSubmit();
     }
-//
+
     public static void clickSubmit() {
-        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
         submitButton.click();
     }
-    public static void provideEmail(String email) {
-        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        emailField.clear();
-        emailField.sendKeys(email);
 
-    }
-    public static void providePassword (String password) {
+    public static void providePassword(String password) {
         WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        wait.until(ExpectedConditions.elementToBeClickable(passwordField));
         passwordField.clear();
         passwordField.sendKeys(password);
+    }
+
+    public static void provideEmail(String email) {
+        WebElement emailField= wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']")));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public static void clickSaveButton() {
+        WebElement saveButton = driver.findElement(By.cssSelector("button.btn-submit"));
+        saveButton.click();
+    }
+
+    public static void provideProfileName(String randomName) {
+        WebElement profileName = driver.findElement(By.cssSelector("[name='name']"));
+        profileName.clear();
+        profileName.sendKeys(randomName);
+    }
+
+    public static void provideCurrentPassword(String password) {
+        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
+        currentPassword.clear();
+        currentPassword.sendKeys(password);
+    }
+
+    public static String generateRandomName() {
+        return UUID.randomUUID().toString().replace("-", "");//
+    }
+
+    public static void clickAvatarIcon() {
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
+        avatarIcon.click();
 
     }
 
+    @DataProvider(name="incorrectLoginProviders")
+    public static Object[][] getDataFromDataproviders() {
+
+        return new Object[][] {
+                {"invalid@email.com", "invalidPass"},
+                {"demo@mail.com", "invalid"},
+                {"", ""}
+        };
+    }
 }
-
-
